5a4cec5a169c61736ca6ee08b9a808883c83fe7c
IgorStarkH <igor.contato97@gmail.com>
2024-03-21 21:49:48 -0300

    adicionado as perguntas dos testes

--- basqueteDeRobos.cs
+++ basqueteDeRobos.cs
@@ -1,3 +1,14 @@
+/*
+A organização da OIBR, Olimpíada Internacional de Basquete de Robô, está começando a ter problemas com dois times:
+os Bit Warriors e os Byte Bulls.
+É que os robôs desses times acertam quase todos os lan- çamentos, de qualquer posição na quadra!
+Pensando bem, o jogo de basquete ficaria mesmo sem graça se jogadores conseguissem acertar qualquer lançamento.
+
+Uma das medidas que a OIBR está implantando é uma nova pontuação para os lançamentos, de acordo com a distância do robô 
+para o início da quadra.
+A quadra tem 2000 centímetros de comprimento, como na figura.
+*/
+
 using System;
 
 class URI
